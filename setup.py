f7cff97410ceb1140bd726347fff3c87ffab2232
Deftry <nikolajkozin15@gmail.com>
2024-01-12 21:42:47 +0700

    bump to 0.1.3 and fix pypi description

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='pythonmommy',
-    version='0.1.1',
+    version='0.1.3',
     author='googer',
     author_email='googer760@gmail.com',
     description='Mommy\'s here to support you when running python~ ❤️',
+    long_description=(Path(__file__).parent/"README.md").read_text(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
