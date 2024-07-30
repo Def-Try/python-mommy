8d350b328df9204ea40dfb02b38628f1d2be9792
VeRaven21 <nikitavoronin953@gmail.com>
2024-07-31 00:26:01 +0500

    Random fix, that's it

--- python_mommy/__init__.py
+++ python_mommy/__init__.py
@@ -16,7 +16,7 @@ MOMMYS_PARTS = os.environ.get("PYTHON_MOMMYS_PARTS", "milk") # anything
 MOMMYS_FUCKING = os.environ.get("PYTHON_MOMMYS_FUCKING", "slut/toy/pet/pervert/whore") # anything
 
 
-MOMMYS_PYTHON = subprocess.run(["which", "python"], capture_output=True).stdout.decode()
+# MOMMYS_PYTHON = subprocess.run(["which", "python"], capture_output=True).stdout.decode()
 
 responses = {
     "chill": {
